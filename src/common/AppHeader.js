05cd0aa5bc508a28033a317590456d31b4f83d30
Francis Makumi <makumi@rheniumgroup.co.ke>
2019-04-30 15:53:46 +0300

    Header change

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -27,7 +27,7 @@ class AppHeader extends Component {
       <Header className="app-header">
         <div className="container">
           <div className="app-title" >
-            <Link to="/">People App</Link>
+            <Link to="/">Premd Ipman</Link>
           </div>
           <Menu
             className="app-menu"
